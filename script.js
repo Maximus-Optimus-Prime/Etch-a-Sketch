ada5d42b100d4abc5c2714ab59cf2f00086f2232
Maximus-Optimus-Prime <maximinbourgeais@gmail.com>
2024-08-21 17:23:52 +0200

    final version

--- script.js
+++ script.js
@@ -1,28 +1,15 @@
 const flexContainer = document.getElementById('flexContainer');
-const button = document.querySelector('button');
-let stylesheet = document.styleSheets[0];
-let myClassRule;
+const button = document.getElementById('btn');
 
 function changeSize() {
     let size = parseInt(prompt("Enter size value (max 100)"));
     size = size > 100 ? 100 : size;
-    console.log(size)
     reset(size);
 }
 
 function reset(size) {
-    let rules = stylesheet.cssRules;
-    for (let i = 0; i < rules.length; i++) {
-        if (rules[i].selectorText === ".flexItem") {
-            myClassRule = rules[i];
-            break;
-        }
-    }
-    let elementsToDelete = document.getElementsByClassName('flexItem');
-    for (let i = 0; i < elementsToDelete.length; i++) {
-        elementsToDelete[i].remove();
-    }
-    myClassRule.style.flex = `1 0 calc(100% / ${size})`;
+    flexContainer.innerHTML = "";
+    flexContainer.style.opacity = 1;
     setSize(size);
 }
 
@@ -31,11 +18,19 @@ function setSize(size) {
         for (let column = 1; column <= size; column++) {
             const flexItem = document.createElement('div');
             flexItem.className = 'flexItem';
+            flexItem.style.backgroundColor = "white";
+            flexItem.style.color = 'black';
+            flexItem.style.flex = `1 0 calc(100% / ${size})`;
             flexItem.textContent = `${row} ${column}`;
+            flexItem.addEventListener('mouseover', () => {
+                flexItem.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+                flexContainer.style.opacity -= 0.1;
+            });
             flexContainer.appendChild(flexItem);
         }
     }
 }
 
 window.onload = function() {reset(5);};
-button.addEventListener("click", function() {reset(10);});
+button.addEventListener("click", changeSize);
+
